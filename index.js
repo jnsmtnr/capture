9431293d60226b178347482ae56068b5b9a4459f
jnstmnr <janosmaitner@gmail.com>
2022-01-24 15:26:10 +0100

    port & try-catch

--- index.js
+++ index.js
@@ -2,14 +2,21 @@ import express, { json } from 'express'
 import cors from 'cors'
 import capture from 'capture-website'
 
+const port = process.env.PORT || 9000
+
 const app = express()
 app.use(cors())
 app.use(json())
 
 app.post('/snap-shot', async (req, res) => {
-    const base64 = await capture.base64(req.body.url)
-
-    res.json({ status: 'ok', base64 })
+    try {
+        const base64 = await capture.base64(req.body.url)
+    
+        res.json({ status: 'ok', base64 })
+    }
+    catch(error) {
+        res.statusCode(500).json({ error: error.message })
+    }
 })
 
-app.listen(9000)
+app.listen(port)
